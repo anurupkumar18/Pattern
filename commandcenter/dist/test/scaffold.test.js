94e4e886ad68ff7821d98f1ba45e880647678b78
Cole Segura <cole.segura@Mac.lan>
2026-07-17 23:56:40 -0600

    chore(cc): exclude generated dependencies

--- commandcenter/dist/test/scaffold.test.js
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { COMMAND_CENTER_VERSION } from "../src/index.js";
-describe("command center scaffold", () => {
-    it("exports a version", () => {
-        expect(COMMAND_CENTER_VERSION).toMatch(/^\d+\.\d+\.\d+$/);
-    });
-});
